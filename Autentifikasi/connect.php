9f01c920d997447d03ef39ec5bd49f0548216765
imanmaliki11 <45257900+imanmaliki11@users.noreply.github.com>
2019-11-13 21:30:48 +0700

    Add files via upload

---
+++ Autentifikasi/connect.php
@@ -0,0 +1,18 @@
+<?php
+class DbConnection{
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'prak_web';
+    protected $connection;
+    public function __construct(){
+        if (!isset($this->connection)) {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+        if (!$this->connection) {
+            echo 'Cannot connect to database server';
+            exit;
+        }
+    }
+    return $this->connection;
+    }
+} ?>
