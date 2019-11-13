487edf0d79c4312e26f0186e799c87bf225e0649
imanmaliki11 <45257900+imanmaliki11@users.noreply.github.com>
2019-11-13 21:18:50 +0700

    Latihan1
    latihan 1 minggu 6

---
+++ Latihan1_Variable.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Contoh skrip PHP</title>
+</head>
+<body>
+    <?php
+    $nama = "Kim Teahyung";
+    echo "$nama berkata \"save me save me\" ";
+    ?>
+</body>
+</html>
